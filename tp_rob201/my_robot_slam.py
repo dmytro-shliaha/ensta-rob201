fe9a08ea505f42572d1801350c679e595a480c1a
dmytro-shliaha <dmytro.shliaha@gmail.com>
2025-04-13 23:51:20 +0200

    Add files via upload

---
+++ tp_rob201/my_robot_slam.py
@@ -0,0 +1,78 @@
+"""
+Robot controller definition
+Complete controller including SLAM, planning, path following
+"""
+import numpy as np
+
+from place_bot.entities.robot_abstract import RobotAbstract
+from place_bot.entities.odometer import OdometerParams
+from place_bot.entities.lidar import LidarParams
+
+from tiny_slam import TinySlam
+
+from control import potential_field_control, reactive_obst_avoid
+from occupancy_grid import OccupancyGrid
+from planner import Planner
+
+
+# Definition of our robot controller
+class MyRobotSlam(RobotAbstract):
+    """A robot controller including SLAM, path planning and path following"""
+
+    def __init__(self,
+                 lidar_params: LidarParams = LidarParams(),
+                 odometer_params: OdometerParams = OdometerParams()):
+        # Passing parameter to parent class
+        super().__init__(should_display_lidar=False,
+                         lidar_params=lidar_params,
+                         odometer_params=odometer_params)
+
+        # step counter to deal with init and display
+        self.counter = 0
+
+        # Init SLAM object
+        # Here we cheat to get an occupancy grid size that's not too large, by using the
+        # robot's starting position and the maximum map size that we shouldn't know.
+        size_area = (1400, 1000)
+        robot_position = (439.0, 195)
+        self.occupancy_grid = OccupancyGrid(x_min=-(size_area[0] / 2 + robot_position[0]),
+                                            x_max=size_area[0] / 2 - robot_position[0],
+                                            y_min=-(size_area[1] / 2 + robot_position[1]),
+                                            y_max=size_area[1] / 2 - robot_position[1],
+                                            resolution=2)
+
+        self.tiny_slam = TinySlam(self.occupancy_grid)
+        self.planner = Planner(self.occupancy_grid)
+
+        # storage for pose after localization
+        self.corrected_pose = np.array([0, 0, 0])
+
+    def control(self):
+        """
+        Main control function executed at each time step
+        """
+        return self.control_tp1()
+
+    def control_tp1(self):
+        """
+        Control function for TP1
+        Control funtion with minimal random motion
+        """
+        self.tiny_slam.compute()
+
+        # Compute new command speed to perform obstacle avoidance
+        command = reactive_obst_avoid(self.lidar())
+        return command
+
+    def control_tp2(self):
+        """
+        Control function for TP2
+        Main control function with full SLAM, random exploration and path planning
+        """
+        pose = self.odometer_values()
+        goal = [0,0,0]
+
+        # Compute new command speed to perform obstacle avoidance
+        command = potential_field_control(self.lidar(), pose, goal)
+
+        return command
